ed4f53a5d06174ed3043b0e69d443623eeaa9a07
nils måsén <nils@piksel.se>
2021-07-03 21:43:41 +0200

    feat(slack): add bot API support

--- pkg/util/jsonclient/jsonclient.go
+++ pkg/util/jsonclient/jsonclient.go
@@ -12,7 +12,7 @@ import (
 const ContentType = "application/json"
 
 // DefaultClient is the singleton instance of jsonclient using http.DefaultClient
-var DefaultClient = &Client{HTTPClient: http.DefaultClient}
+var DefaultClient = NewClient()
 
 // Get fetches url using GET and unmarshals into the passed response using DefaultClient
 func Get(url string, response interface{}) error {
@@ -26,7 +26,13 @@ func Post(url string, request interface{}, response interface{}) error {
 
 // Client is a JSON wrapper around http.Client
 type Client struct {
-	HTTPClient *http.Client
+	HTTPClient          *http.Client
+	AuthorizationHeader string
+	Indent              string
+}
+
+func NewClient() *Client {
+	return &Client{HTTPClient: http.DefaultClient}
 }
 
 // Get fetches url using GET and unmarshals into the passed response
@@ -41,17 +47,26 @@ func (c *Client) Get(url string, response interface{}) error {
 
 // Post sends request as JSON and unmarshals the response JSON into the supplied struct
 func (c *Client) Post(url string, request interface{}, response interface{}) error {
-
 	var err error
 	var body []byte
 
-	body, err = json.Marshal(request)
+	body, err = json.MarshalIndent(request, "", c.Indent)
 	if err != nil {
 		return fmt.Errorf("error creating payload: %v", err)
 	}
 
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("error creating request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", ContentType)
+	if c.AuthorizationHeader != "" {
+		req.Header.Set("Authorization", c.AuthorizationHeader)
+	}
+
 	var res *http.Response
-	res, err = c.HTTPClient.Post(url, ContentType, bytes.NewReader(body))
+	res, err = c.HTTPClient.Do(req)
 	if err != nil {
 		return fmt.Errorf("error sending payload: %v", err)
 	}
